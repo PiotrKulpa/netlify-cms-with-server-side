0532386b03fd38c6af8277b3bd936de7263c7fc6
unknown <info.kulpa@gmail.com>
2020-11-26 21:23:11 +0100

    update gitignore

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,10 @@
 import Layout from '../components/layout'
 import { attributes, html } from '../content/about.md'
 
-const About = () => (
+const About = ({ test}) => (
   <Layout>
     <h1>{attributes.title}</h1>
+    <h2>{ test }</h2>
     <div dangerouslySetInnerHTML={{ __html: html }} />
     <style jsx>{`
       h1,
@@ -13,5 +14,13 @@ const About = () => (
     `}</style>
   </Layout>
 )
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      test: 'Testing server side rendering on netlify'
+    }, // will be passed to the page component as props
+  }
+}
 
 export default About
