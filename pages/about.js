68eb0d5e152217c2b55ab649b1ea2c7bc960038f
unknown <info.kulpa@gmail.com>
2020-11-27 22:45:08 +0100

    testing airtable client

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
 import Layout from '../components/layout'
 import { attributes, html } from '../content/about.md'
+var Airtable = require('airtable');
+let fetchedMenu = [];
 
-const About = ({ test, testEnv }) => (
+const About = ({ test,  fetchedMenu = [] }) => (
   <Layout>
     <h1>{attributes.title}</h1>
     <h2>{ test }</h2>
-    <h2>{ testEnv }</h2>
+    { fetchedMenu &&  fetchedMenu.map((el) => <p>{el.name}</p>)}
     <div dangerouslySetInnerHTML={{ __html: html }} />
     <style jsx>{`
       h1,
@@ -18,12 +20,31 @@ const About = ({ test, testEnv }) => (
 
 export async function getServerSideProps(context) {
 
-  const testEnv = process.env.TEST;
+  const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
+
+
+  var base = new Airtable({apiKey: AIRTABLE_API_KEY}).base('apppv5xeZ1bAJdvLy');
+ 
+  base('Menu').select({
+    view: 'Grid view',
+  }).firstPage((err, records) => {
+
+    if (err) { console.error(err); return; }
+    const menu = records.map((record) => {
+      return {
+        name: record.get('nameVisible'),
+        url: record.get('menuUrl'),
+        slug: record.get('slug'),
+      }
+    });
+    fetchedMenu = menu;
+    
+  });
 
   return {
     props: {
       test: 'Testing server side rendering on netlify',
-      testEnv
+      fetchedMenu
     }, // will be passed to the page component as props
   }
 }
